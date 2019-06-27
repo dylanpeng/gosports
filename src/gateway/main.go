c5e5ce4d4f17f27ce3d8ea799cce2eb8748ece34
peng.chen <peng.chen@opay-inc.com>
2019-06-27 17:43:30 +0800

    2019-06-27 Add flag params

--- src/gateway/main.go
+++ src/gateway/main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gosports/gateway/config"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+var configFilePath = flag.String("c", "gateway.toml", "config file path")
+
 func main() {
-	err := config.Init("/Users/chenpeng/working/gosports/src/conf/gateway.toml")
+	//parse flag
+	flag.Parse()
+
+	err := config.Init(*configFilePath)
 	if err != nil{
-		fmt.Printf("Init config failed! err: %s", err)
+		fmt.Printf("Init config failed! err: %s \n", err)
 	}
 
 	conf := config.GetConfig()
-	fmt.Printf("config: %+v", conf)
+	fmt.Printf("config: %+v \n", conf)
+
+	waitForSignal()
+}
+
+func waitForSignal(){
+	// waitting for exit signal
+	exit := make(chan os.Signal)
+	stopSigs := []os.Signal{
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		syscall.SIGABRT,
+		syscall.SIGKILL,
+		syscall.SIGTERM,
+	}
+	signal.Notify(exit, stopSigs...)
+
+	// catch exit signal
+	sign := <-exit
+	fmt.Printf("stop by exit signal '%s'", sign)
 }
